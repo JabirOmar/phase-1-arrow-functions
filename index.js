bcf320e7e8047c8fbee22a474a6411f79742f236
Jabir Omar <jabiromar400@gmail.com>
2022-08-18 06:52:05 +0300

    Completed

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = function (){
+    let num1 = 2000;
+    let num2 = 100;
+    let division = num1 / num2;
+
+    square = (a) => {
+        return a * a
+    }
+
+    add = (b, c) => {
+        return b + c
+    }
+
+    return division 
+}
+console.log(divide())
